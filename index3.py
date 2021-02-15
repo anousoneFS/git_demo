fc739a06bb835be8c35a33dd7637feed6ea9a21c
anousoneFS <sone.freestyle7898@gmail.com>
2021-02-15 17:14:36 +0700

    last commit

--- index3.py
+++ index3.py
@@ -1,14 +1,4 @@
 print("hi alllll")
 print("hi alllll")
 
-<<<<<<< HEAD
 print("add feature")
-=======
-
-
-
-def fullname(first_name, last_name):
-    return "hello "+ first_name " " + last_name
-
-print(fullname("anousone", "worlakoumman"))
->>>>>>> feature3
